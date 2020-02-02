f45e0e82e968c196ecc4c07e8031e0deafa60d1e
Enrico Gerez Camponogara Da Silva <camponogara5@gmail.com>
2020-02-02 00:04:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const functions = require('firebase-functions');
 const admin = require("firebase-admin");
 
 const serviceAccount = require("./serviceAccountKey.json");
-
+// this code is find on console.firebase.google.com/yourproject on settings
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://todomylist-ccd88.firebaseio.com"
+  databaseURL: "your-database-url"
 });
 
 const graphql = require("./src/graphql");
